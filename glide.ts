fee2b4d35af68b85ded0be514500e6a7646ed466
Bruno A. Muciño <mucinoab@gmail.com>
2025-08-02 21:47:49 -0600

    Glide config

---
+++ glide.ts
@@ -0,0 +1,131 @@
+// Config docs:
+//
+//   https://glide-browser.app/config
+//
+// API reference:
+//
+//   https://glide-browser.app/api
+//
+// Default config files can be found here:
+//
+//   https://github.com/glide-browser/glide/tree/main/src/glide/browser/base/content/plugins
+//
+// Most default keymappings are defined here:
+//
+//   https://github.com/glide-browser/glide/blob/main/src/glide/browser/base/content/plugins/keymaps.mts
+//
+// Try typing `glide.` and see what you can do!
+//
+
+// =============== AUTOCMDS ===============
+
+glide.autocmds.create("ModeChanged", "*", ({ new_mode }) => {
+	let color = null;
+
+	switch (new_mode) {
+		case "insert":
+			color = "#d8b9e7";
+			break;
+
+		case "normal":
+			color = "#f2f3f4";
+			break;
+
+		case "command":
+			color = "#b9c8e7";
+			break;
+
+		case "hint":
+			color = "#fffacd";
+			break;
+	}
+
+	if (color) {
+		browser.theme.update({ colors: { frame: color } });
+	}
+});
+
+interface TabInfo {
+	tabId: number;
+	url: string;
+}
+
+class TabTracker {
+	private currentTab: TabInfo | null = null;
+	private latestTab: TabInfo | null = null;
+
+	constructor() {
+		glide.autocmds.create("UrlEnter", /.+/, (event) => {
+			this.update(event.tab_id, event.url);
+		});
+	}
+
+	public update(tabId: number, url: string): void {
+		if (this.currentTab && this.currentTab.tabId === tabId) {
+			this.currentTab.url = url;
+			return;
+		}
+
+		if (this.currentTab) {
+			this.latestTab = this.currentTab;
+		}
+
+		this.currentTab = { tabId, url };
+	}
+
+	public getLatest(): TabInfo | null {
+		return this.latestTab;
+	}
+}
+
+const tabTracker = new TabTracker();
+
+// =============== KEYMAPS ===============
+
+glide.keymaps.set(
+	"normal",
+	"<Space><Tab>",
+	() => glide.excmds.execute("tab_next"),
+	{ description: "leader + tab to cycle tabs" },
+);
+
+glide.keymaps.set("normal", "<C-n>", () => glide.excmds.execute("tab_next"), {
+	description: "crl + n to cycle tabs",
+});
+
+// =============== TABS ===============
+
+async function go_to_tab(url: string) {
+	const tab = await glide.tabs.get_first({ url });
+	assert(tab && tab.id);
+
+	await browser.tabs.update(tab.id, { active: true });
+}
+
+glide.keymaps.set(
+	"normal",
+	"gcc",
+	() => go_to_tab("https://outlook.office.com/*"),
+	{
+		description: "[g]o to [c]alendar",
+	},
+);
+
+glide.keymaps.set("normal", "gss", () => go_to_tab("https://app.slack.com/*"), {
+	description: "[g]o to [s]lack",
+});
+
+glide.keymaps.set(
+	"normal",
+	"<Space><Space>",
+	async () => {
+		const tab = tabTracker.getLatest();
+
+		if (tab) {
+			await browser.tabs.update(tab.tabId, { active: true });
+		}
+	},
+	{
+		description: "switch to latest tab",
+	},
+);
