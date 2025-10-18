98a8485b96e2e904ad0310881cb8c33e2a5bc40a
Bruno A. Muciño <mucinoab@gmail.com>
2025-10-17 22:25:51 -0600

    arch fixes

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,30 +1,31 @@
 return {
   'nvim-telescope/telescope.nvim',
   lazy = false,
-  dependencies = { 
+  dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-fzy-native.nvim',
   },
-  opts = {
-    defaults = {
-      layout_config = {
-        vertical = { width = 0.99 }
+  config = function()
+    local actions = require('telescope.actions')
+    require('telescope').setup({
+      defaults = {
+        layout_config = {
+          vertical = { width = 0.99 }
+        },
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case'
+        },
+        winblend = 15,
+        mappings = { i = { ["<esc>"] = actions.close } },
+        set_env = { ['COLORTERM'] = 'truecolor' },
       },
-      vimgrep_arguments = {
-        'rg',
-        '--color=never',
-        '--no-heading',
-        '--with-filename',
-        '--line-number',
-        '--column',
-        '--smart-case'
-      },
-      winblend = 15,
-      mappings = { i = { ["<esc>"] = require('telescope.actions').close } },
-      set_env = { ['COLORTERM'] = 'truecolor' },
-    },
-  },
-  init = function()
-	  require('telescope').load_extension('fzy_native')
+    })
+    require('telescope').load_extension('fzy_native')
   end,
 }
