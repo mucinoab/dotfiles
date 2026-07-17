5212d6b4727bd2f09f96170e3b65e09fb215dc15
Bruno A. Muciño <mucinoab@gmail.com>
2026-07-16 21:52:12 -0600

    Add automatic light/dark theme switching with darkman
    darkman flips the system color-scheme at sunset/sunrise (Mexico City)
    through gsettings hooks; Ghostty, delta, and nvim (auto-dark-mode.nvim,
    github_light/github_dark_default) all follow it.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,7 +4,11 @@ return {
   priority = 1000,
   config = function()
     require('github-theme').setup()
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      callback = function()
+        vim.cmd('highlight Whitespace guifg=#e0a0a0')
+      end,
+    })
     vim.cmd('colorscheme github_light')
-    vim.cmd('highlight Whitespace guifg=#e0a0a0')
   end
 }
