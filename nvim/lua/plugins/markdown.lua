12bb383719858140866895bad521544ea4e6e804
Bruno A. Muciño <mucinoab@gmail.com>
2026-08-19 11:12:38 -0600

    nvim: render markdown tables, drop outline sidebar

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,9 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  ft = "markdown",
+  opts = {
+    pipe_table = {
+      preset = "round",
+    },
+  },
+}
--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -134,34 +134,6 @@ return {
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
-      {
-        "hedyhli/outline.nvim",
-        dependencies = { 'epheien/outline-treesitter-provider.nvim' },
-        ft = "markdown",
-        opts = {
-          outline_window = { position = "right", width = 25, },
-          preview_window = { auto_preview = false, },
-          symbol_folding = { autofold_depth = 5, },
-          providers = { priority = { 'treesitter' } },
-        },
-        config = function(_, opts)
-          require("outline").setup(opts)
-
-          vim.api.nvim_create_autocmd("BufEnter", {
-            callback = function()
-              if vim.bo.filetype == "markdown" then
-                vim.schedule(function()
-                  local outline = require("outline")
-                  if not outline.is_open() then
-                    outline.open()
-                    outline.focus_code()
-                  end
-                end)
-              end
-            end,
-          })
-        end,
-      },
     },
     opts = {
       legacy_commands = false,
